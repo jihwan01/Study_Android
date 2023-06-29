f85c39a738bc992a7cc99065665bbe9bbe0c714e
jihwan <dhwlghksy@gmail.com>
2023-06-30 00:11:25 +0900

    Make Custom List View

--- app/src/main/java/com/example/study_listview/MainActivity.java
+++ app/src/main/java/com/example/study_listview/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.study_listview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listMenu);
+        ListView listview ;
+        ListViewAdapter adapter;
 
-        ListView listView = (ListView) findViewById(R.id.listview1);
-        listView.setAdapter(adapter);
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
 
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.kakao_logo),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.jihwan),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.kakao_logo),
+                "Ind", "Assignment Ind Black 36dp") ;
     }
 
     static final String[] listMenu = {"list4", "list5", "list6"};
